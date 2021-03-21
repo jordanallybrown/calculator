408db3b857606d79a111a13409c464e371c865d4
jordanallybrown <jordanallybrown@gmail.com>
2021-03-20 23:57:48 -0400

    Add functionality to display expression w/ ints, operators, backspace, and clear

--- src/view.mjs
+++ src/view.mjs
@@ -1,10 +1,11 @@
 const KEYS = [
     'C', '+/-', '%', '/', 
-    '7', '8', '9', 'x', 
+    '7', '8', '9', '*', 
     '4', '5', '6', '-', 
     '1', '2', '3', '+', 
     '0', '.', 'b', '='
-]
+];
+
 
 export default class View {
 
@@ -18,23 +19,13 @@ export default class View {
         this.consoleExpression = this.createElement('div', '', 'expression');
         this.consoleResult = this.createElement('div', '', 'result');
         this.console.append(this.consoleExpression, this.consoleResult);
+
+        // Initialize an output parser to capture the current state of the console
+        this.outputParser = new _OutputParser();
         
-
+        // Add all console divs and button keys to calculator grid
         this.calculatorGrid.append(this.console, this._createKeys());
-
-        // captures the state of the console
-        this.properties = {
-            expression: [],
-            result: ""
-        };
-    }
-
-    updateConsole(){
-
-    }
-
-    displayOutput(){
-
+        
     }
 
     createElement(tag, textContent="", className=""){
@@ -49,6 +40,12 @@ export default class View {
         return element;
     }
 
+    updateConsole(){ // updates both the expression and the result
+        // console.log(this.outputParser.expression);
+        this.consoleExpression.textContent = this.outputParser.expression;
+    }
+
+
     _createKeys(){
 
         const fragment = document.createDocumentFragment();
@@ -61,14 +58,14 @@ export default class View {
             switch (key) {
                 case 'C':
                     keyElement.addEventListener('click', () => {
-                        this.properties.expression = [];
-                        console.log(this.properties.expression);
+                        this.outputParser.clear();
+                        this.updateConsole();
                     });
                     break;
                 case 'b':
                     keyElement.addEventListener('click', () => {
-                        this.properties.expression.pop();
-                        console.log(this.properties.expression);
+                        this.outputParser.pop();
+                        this.updateConsole();
                     });
                     break;
                 case '+/-':
@@ -76,8 +73,8 @@ export default class View {
                 case '=':
                 default:
                     keyElement.addEventListener('click', () => {
-                        this.properties.expression.push(key);
-                        console.log(this.properties.expression);
+                        this.outputParser.append(key);
+                        this.updateConsole();
                     });
                     break;
                 
@@ -91,6 +88,55 @@ export default class View {
                 
     }
 
+}
+
+class _OutputParser {
+    constructor(){
+        this._expression = [];
+        this._result = "";
+    }
+
+    get expression() {
+        return this._expression.join("");
+    }
+    get result(){
+        return this._result;
+    }
+    set result(operand){
+        this._result = operand;
+    }
+
+    _isDigit(char){
+        return !(char.search(/\d/g) === -1);
+    }
+
+    append(input){
+        if (this._isDigit(input)){
+            this._expression.push(input);
+        }
+        else{ // input is an operator
+            if (this._expression.length > 0){ // Only push operator if list is not empty
+                let prevInput = this._expression[this._expression.length - 1];
+                if (this._isDigit(prevInput)){ // If prev is digit, push operator
+                    this._expression.push(input);
+                }
+                else { // If prev is operator, replace the prev operator w/ new operator
+                    this._expression.pop();
+                    this._expression.push(input);
+
+                }
+            }
+
+        }
+    }
+
+    clear(){
+        this._expression = [];
+    }
+
+    pop(){
+        this._expression.pop();
+    }
 
 
 
