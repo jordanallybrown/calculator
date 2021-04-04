51e3f2875a3ab5094265e1fc605d6d5dfb533a42
jordanallybrown <jordanallybrown@gmail.com>
2021-04-04 16:27:24 -0400

    Fixed #3 Needs refactoring, but now append handlers working for diff buttons

--- src/view.mjs
+++ src/view.mjs
@@ -6,6 +6,7 @@ const KEYS = [
     '0', '.', 'backspace', '='
 ];
 
+const OPERATORS = ['/', '*', '-', '+', '%'];
 
 export default class View {
 
@@ -43,6 +44,8 @@ export default class View {
     updateConsole(){ // updates both the expression and the result
         // console.log(this.outputParser.expression);
         this.consoleExpression.textContent = this.outputParser.expression;
+        console.log('The current console list is:');
+        console.log(this.outputParser.displayExpression());
     }
 
 
@@ -59,7 +62,7 @@ export default class View {
                 const iconElement = this.createElement("i", "backspace", "material-icons");
                 keyElement.appendChild(iconElement);
                 keyElement.addEventListener('click', () => {
-                    this.outputParser.pop();
+                    this.outputParser.backspace();
                     this.updateConsole();
                 });
                 
@@ -77,15 +80,36 @@ export default class View {
                             this.updateConsole();
                         });
                         break;
-                    case '+/-':
                     case '.':
-                    case '=':
-                    default:
                         keyElement.addEventListener('click', () => {
-                            this.outputParser.append(key);
+                            this.outputParser.appendDecimal(key);
                             this.updateConsole();
                         });
                         break;
+                    case '+/-':
+                        keyElement.addEventListener('click', () => {
+                            this.outputParser.appendMinus(key);
+                            this.updateConsole();
+                        });
+                        break;
+                    case '=':
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '%':
+                        keyElement.addEventListener('click', () => {
+                            this.outputParser.appendOperator(key);
+                            this.updateConsole();
+                        });
+                        break;
+                    default:
+                        keyElement.addEventListener('click', () => {
+                            this.outputParser.appendOperand(key);
+                            this.updateConsole();
+                        });
+                        break;
+
                     
                 }
 
@@ -102,12 +126,36 @@ export default class View {
 
 }
 
+class _Digit {
+
+    constructor(value){
+        this._value = value;
+        this._isNegative = false;
+    }
+
+    hasDecimal(){
+        return !(this._value.search('.') === -1)
+    }
+
+    get isNegative(){
+        return this._isNegative;
+    }
+
+    toggleSign(){
+        this._isNegative = !this._isNegative;
+    }
+
+}
+
 class _OutputParser {
     constructor(){
         this._expression = [];
         this._result = "";
     }
 
+    displayExpression(){
+        return this._expression;
+    }
     get expression() {
         return this._expression.join("");
     }
@@ -118,28 +166,87 @@ class _OutputParser {
         this._result = operand;
     }
 
+    getPreviousChar(){
+        if(this._expression.length > 0){
+            return this._expression[this._expression.length - 1];
+        }
+            
+        else{
+            return null;
+        }
+        
+    }
+
+    updatePreviousChar(char){
+        return this._expression[this._expression.length - 1] = char;
+    }
+
     _isDigit(char){
         return !(char.search(/\d/g) === -1);
     }
 
-    append(input){
-        if (this._isDigit(input)){
-            this._expression.push(input);
-        }
-        else{ // input is an operator
-            if (this._expression.length > 0){ // Only push operator if list is not empty
-                let prevInput = this._expression[this._expression.length - 1];
-                if (this._isDigit(prevInput)){ // If prev is digit, push operator
-                    this._expression.push(input);
-                }
-                else { // If prev is operator, replace the prev operator w/ new operator
-                    this._expression.pop();
-                    this._expression.push(input);
 
-                }
+    //TODO: Have to handle negatives at some point, not sure how this is working atm
+
+    appendMinus(input){
+        let prev = this.getPreviousChar();
+        
+
+        if( prev !== null && !(OPERATORS.includes(prev)) ){ 
+            // Check if minus sign has already been applied
+            const isNegative = prev.includes('-');
+
+            if(isNegative){ // remove the negative sign, should always be first index
+                prev = prev.replace('-', '');
             }
+            else { // add in the negative sign at the beginning of the string
+                prev = '-' + prev;
+            }
+            this.updatePreviousChar(prev);
+        } 
 
-        }
+    }
+
+    appendOperator(input){
+        // console.log('We need to append operand');
+        const prev = this.getPreviousChar();
+
+        // If the console is blank or previous char was an operator, do not add, else add it to separate string
+        if( !(OPERATORS.includes(prev) || prev === null)){ 
+            this._expression.push(input);
+        } 
+
+    }
+
+    appendOperand(input){
+        // console.log('We need to append operand');
+        const prev = this.getPreviousChar();
+       
+        // The console is blank, the previous char was an operator, just add to expression list
+        if(OPERATORS.includes(prev) || prev === null){ 
+            this._expression.push(input);
+        } 
+        else { // The previous char was an operand or decimal, so we need to aggregate with the previous
+            this.updatePreviousChar(prev + input);
+        }  
+    }
+
+    appendDecimal(input){
+        
+        const prev = this.getPreviousChar()
+       
+        // The console is blank, the previous char was an operator, just add to expression list
+        if(OPERATORS.includes(prev) || prev === null){ 
+            this._expression.push(input);
+            
+        } 
+        else { // The previous char was an operand or decimal, so we need to aggregate with the previous
+            // Ensure that a decimal is not already present, if it is don't aggregate
+            if(!prev.includes('.')){ // no decimals
+                this.updatePreviousChar(prev + input);
+            }
+            
+        }  
     }
 
     clear(){
@@ -148,6 +255,20 @@ class _OutputParser {
 
     pop(){
         this._expression.pop();
+    }
+
+    backspace(){
+        let prev = this.getPreviousChar();
+
+        // Ensure there are items to delete
+        if(prev.length > 1){ // An operand that has multiple chars
+
+            prev = prev.slice(0, prev.length-1); // remove the last char only
+            this.updatePreviousChar(prev);
+        }
+        else{ // if char is just one length, simply remove it
+            this._expression.pop();
+        }
     }
 
 
